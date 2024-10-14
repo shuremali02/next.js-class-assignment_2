566a5ed80a04ab45950f6bbb049380e906eadf0b
shuremali02 <shuremsyed41@gmail.com>
2024-10-13 22:18:13 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Herosec from './components/Herosection/Herosec'
-import Services from './components/Services/service'
+import Services from './components/Services/Services'
 import Steps from './components/Ordersection/Ordersec'
-import Testimonials from './components/ClientTestimonials/testimonials'
-import Contact from './components/Contact/contact'
+import Testimonials from './components/ClientTestimonials/Testimonial'
+import Contact from './components/Contact/Contactus'
 
 export default function HomePage() {
   return (
